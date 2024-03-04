f77f593d5c97b42daace65ae190f7d05d41585d7
rayane <moryane900@gmail.com>
2024-03-04 21:25:15 +0100

    DEHO basic functionnalities done needs : tunninh funciton or GA to optimise parameters use index arrays instead of binary ones try and add new representation for a solution

--- preprocess.py
+++ preprocess.py
@@ -40,5 +40,55 @@ def elephant_to_dict(binary_array, input_dict):
             filtered_dict[i] = input_dict[i]
     return filtered_dict
 
+def binary_to_index(binary_array):
+    index_array = []
+    for i, bit in enumerate(binary_array, start=1):
+        if bit == 1:
+            index_array.append(i)
+    return index_array
 
+def get_values(binary_array, input_dict):
+    values = []
+    count = 0
+    for i, bit in enumerate(binary_array, start=1):
+        if bit == 1:
+            values.append(input_dict[i])
+            count += 1
+    return values
+
+def distance_manhattan(centroid, datapoint):
+    centroid = np.array(centroid)
+    datapoint = np.array(datapoint)
+    dist = np.sum(np.abs(centroid - datapoint))
+    return dist
+
+def distance_minkowski(centroid, datapoint, p):
+    centroid = np.array(centroid)
+    datapoint = np.array(datapoint)
+    dist = np.sum(np.abs(centroid - datapoint) ** p)
+    return (dist ** (1/p))
+
+def distance_euclidean(centroid, datapoint):
+    centroid = np.array(centroid)
+    datapoint = np.array(datapoint)
+    dist = np.sqrt(np.sum((centroid - datapoint)**2))
+    return dist
+
+def distance_cosine(centroid, datapoint):
+    centroid = np.array(centroid)
+    datapoint = np.array(datapoint)
+    up = np.sum(centroid * datapoint)
+    down1 = np.sum(centroid * centroid)
+    down2 = np.sum(datapoint * datapoint)
+    dist = 1 - (up / (np.sqrt(down1) * np.sqrt(down2)))
+    return dist
+
+def distance_hamming(centroid, datapoint):
+    centroid = np.array(centroid)
+    datapoint = np.array(datapoint)
+    dist =0
+    for c,d in zip(centroid, datapoint):
+        if c != d:
+            dist += 1
+    return dist
 
